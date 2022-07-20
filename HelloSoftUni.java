53cf5d904d7c58b3cfd3a46942690e297ee12455
Angel <107347177+AngelGatev@users.noreply.github.com>
2022-07-20 06:44:33 +0300

    Add files via upload

---
+++ HelloSoftUni.java
@@ -0,0 +1,11 @@
+package PbJavaNovember.src.FirstStepsInCoding.Lab;
+public class HelloSoftUni {
+    public static void main(String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+}
+
+
+
+
+
